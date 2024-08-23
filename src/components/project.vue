5c6f166a3485bc9a3529962716958c310cd671af
lzx <503806316@qq.com>
2024-08-24 07:20:47 +0800

    第二次更新

---
+++ src/components/project.vue
@@ -0,0 +1,48 @@
+<script setup></script>
+
+<template>
+  <div class="flex-container">
+    <div class="flex-item">
+      <router-link to="/FlexSystem" class="el-link el-link--primary">ProjectOne：HTML+CSS flex布局系统</router-link>
+    </div>
+    <div class="flex-item">flex item 2</div>
+    <div class="flex-item">flex item 3</div>
+    <div class="flex-item">flex item 3</div>
+    <div class="flex-item">flex item 3</div>
+    <div class="flex-item">flex item 3</div>
+    <div class="flex-item">flex item 3</div>
+    <div class="flex-item">flex item 3</div>
+    <div class="flex-item">flex item 3</div>
+    <div class="flex-item">flex item 3</div>
+  </div>
+</template>
+
+<style scoped>
+.flex-container {
+  margin: 30px auto;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 1400px;
+}
+.flex-container::after {
+  content: "";
+  height: 0;
+  /* 和内容项的宽度保持一致 */
+  width: 300px;
+}
+.flex-item {
+  margin: 30px;
+  width: 290px;
+  height: 200px;
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+  line-height: 200px;
+
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px 0px;
+}
+</style>
